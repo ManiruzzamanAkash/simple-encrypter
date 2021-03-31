c0bde60607a4fc388355012dfcc6ceab56168566
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-04-01 01:55:10 +0600

    First Commit of Simple Encrypter

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Encrypt String
+ * 
+ * @param string string Unencripted string which needs to change
+ * 
+ * @return string encrypted string
+ */
+function encryptString(string) {
+    const privateSecret = "aa%cd(e$fgh1*23@45^6@";
+    let encryptedString = privateSecret.split("")[0] + string.length;
+    const stringParts = string.split("");
+    const secretParts = privateSecret.split("");
+
+    // Add string parts inside the private secret parts
+    for (let i = 0; i < secretParts.length; i++) {
+        const text = secretParts[i];
+
+        encryptedString += text;
+
+        if (typeof stringParts[i] !== "undefined") {
+            encryptedString += stringParts[i];
+        }
+    }
+
+    return encryptedString;
+}
+
+/**
+ * Decrypt String
+ * 
+ * @param string encrypted string needs to unencrypt
+ * 
+ * @return string
+ */
+function decryptString(string) {
+    let decryptedString = "";
+    const encryptedLength = string.split("")[1];
+    const totalVisitedCharacter = 3 + (encryptedLength % 2 !== 0 ? encryptedLength * 2 : encryptedLength * 2 - 1) - 1;
+    const encryptedStringParts = string.split("");
+
+    for (let index = 0; index < encryptedStringParts.length; index++) {
+        const es = encryptedStringParts[index];
+
+        if (index >= 3 && index <= totalVisitedCharacter) {
+            if ((index + 1) % 2 === 0) {
+                decryptedString += es;
+            }
+        }
+    }
+
+    return decryptedString;
+}
+
+// Exports two functions
+module.exports = {
+    encryptString: encryptString,
+    decryptString: decryptString,
+}
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Simple Encrypter `Javascript`
+
+Simple Encrypter npm package, that encrypt and descrypt a string. It's super simple and fast javascript text encrypter.
+
+---
+
+
+## Usage:
+```js
+npm install simple-encrypter
+```
+
+### How to encrypt a string ?
+```js
+const { encryptString } = require( 'simple-encrypter');
+console.log(`encrypt`, encryptString('akash'));
+
+```
+
+### How to decrypt a string ?
+```js
+const { decryptString } = require( 'simple-encrypter');
+console.log(`decrypt`, decryptString('a5aaak%acsdh(e$fgh1*23@45^6@'));
+```
+
+## Full Encrypt & Decrypt string Example:
+
+```js
+const { encryptString, decryptString } = require( 'simple-encrypter');
+
+console.log(encryptString('akash')); // a5aaak%acsdh(e$fgh1*23@45^6@
+console.log(decryptString('a5aaak%acsdh(e$fgh1*23@45^6@')); // akash
+
+```
+
+Yeah, It's super simple. right..?
+
+## License:
+MIT
+
+## Contributor:
+Md. Maniruzzaman Akash <manirujjamanakash@gmail.com> (https://akash.devsenv.com)
